cd49fe5a799b554c68c54d46c1a11267c0507123
lesyeuxdekuma <129199707+lesyeuxdekuma@users.noreply.github.com>
2023-05-06 23:52:44 +0700

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Jennie Kim",
+    imgURL: "jennie.jpg",
+    phone: "+62 890 9077 1996",
+    email: "kumadenini@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Park Chaeoung",
+    imgURL: "rose.jpg",
+    phone: "+62 823 7782 1997",
+    email: "juhwhank@@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Jisoo Kim",
+    imgURL: "jisoo.jpg",
+    phone: "+62 890 9077 1996",
+    email: "kumadenini@gmail.com"
+  }
+];
+
+export default contacts;
